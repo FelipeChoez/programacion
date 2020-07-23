72202f1780f99b5c20773ba323dd13a37de9db7a
felipe choez <felipe_choez16@hotmail.com>
2020-07-23 15:57:08 -0500

    agregar nuevo main

--- Estudiante_online/src/conn/estudiantesonline/Main.java
+++ Estudiante_online/src/conn/estudiantesonline/Main.java
@@ -1,16 +1,27 @@
 package conn.estudiantesonline;
 
-
-
 import java.util.List;
 
-//import com.cursoDAO.CursoDAOImpl;
+import conn.DAO.Curso_estudianteDAOImpl;
 import conn.DAO.estudianteDAOImpl;
-
-//import conn.entidades.Cursos;
-import conn.entidades.Estudiante;
-//import conn.interfa.CursosDAO;
+import conn.entidades.Curso_estudiantes;
+import conn.entidades.Cursos;
+import conn.interfa.Curso_estudianteDAO;
 import conn.interfa.EstudianteDAO;
+
+//import java.util.List;
+
+//import conn.DAO.CursoDAOImpl;
+//import conn.DAO.estudianteDAOImpl;
+//import conn.entidades.Cursos;
+//import com.cursoDAO.CursoDAOImpl;
+//import conn.DAO.estudianteDAOImpl;
+
+//import conn.entidades.Estudiante;
+//import conn.interfa.CursosDAO;
+//import conn.interfa.EstudianteDAO;
+//import conn.interfa.CursosDAO;
+//import conn.interfa.EstudianteDAO;
 
 public class Main {
 	  static String URL = "jdbc:postgresql://localhost:5432/estudianteOnline";
@@ -20,9 +31,34 @@ public class Main {
 	  
 	  public static void main(String[] args) {
 		  
-             //CursosDAO cursoDAO = new CursoDAOImpl();
+		  EstudianteDAO estudianteDAO = new estudianteDAOImpl();
+	       Curso_estudianteDAO curso_estudiante = new Curso_estudianteDAOImpl();
+	       Curso_estudiantes curso_estudiantes = new Curso_estudiantes(0,5,4);
+	       curso_estudiante.ingresar(curso_estudiantes);
+		   System.out.println("ingresado");
+		    
+		  
+		    
+		  
+		      //CursosDAO cursoDAO = new CursoDAOImpl();
+             //List<Estudiante> estudiantes = cursoDAO.getCursosPorCurso(1);
+             //System.out.println(estudiantes);
+            
+             //EstudianteDAO estudianteDAO = new estudianteDAOImpl();
+
+             //List<Cursos> curso1 = estudianteDAO.getCursosPorEstudiante(2);
+             //List<Cursos> curso2 = estudianteDAO.getCursosPorEstudiante(1);
+              
+             //System.out.println(curso1);
+             //System.out.println(curso2);
              
-            //Cursos curso = new Cursos(1, "programacion IV");
+             
+             
+             
+             
+             
+             
+             //Cursos curso = new Cursos(1, "programacion IV");
             //cursoDAO.ingresar(curso);
            //cursoDAO.eliminar(2);
              //cursoDAO.actualizar(curso);
@@ -36,7 +72,7 @@ public class Main {
              //System.out.println(cursos.size());
              
           
-		        EstudianteDAO estudianteDAO = new estudianteDAOImpl();
+		        //EstudianteDAO estudianteDAO = new estudianteDAOImpl();
 		        //Estudiante estudiante =  new Estudiante(2, "juan", "fernandez", "juanfernandez@gmail.com");
 		        
 		        //estudianteDAO.ingresar(estudiante);
@@ -46,8 +82,8 @@ public class Main {
 		  
 		        
 		        
-	             List<Estudiante> estudiantes = estudianteDAO.getEstudiante(); 
-	             System.out.println(estudiantes);
+	             //List<Estudiante> estudiantes = estudianteDAO.getEstudiante(); 
+	             //System.out.println(estudiantes);
 	
 	}
 
